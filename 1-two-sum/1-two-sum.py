262db318b0300e2d8984c943ba18b709268d14a9
Ravikrishna234 <41903289+Ravikrishna234@users.noreply.github.com>
2022-09-14 13:39:55 -0500

    Time: 1709 ms (29.59%), Space: 15 MB (76.47%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -6,18 +6,5 @@ class Solution:
                 my_dict[i] = nums[i]
             if(len(my_dict) and target-nums[i] in my_dict.values()):
                 my_dict[i] = nums[i]
-            #elif(len(my_dict) and target-nums[i] in my_dict.values()):
-                #my_dict[i] = nums[i]
-            #else:
-                #pass
             
         return list(my_dict.keys())
-#class Solution:
-    #def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #my_dict = {}
-        #for i in range(len(nums)):
-            #if(len(my_dict) and target-nums[i] in my_dict.values()):
-                #my_dict[i] = nums[i]
-            #else:
-                #my_dict[i] = nums[i]
-        #return list(my_dict.keys())

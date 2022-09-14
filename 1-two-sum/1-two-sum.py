8951bca78888869796330a402cbc64cb2e2bebf5
Ravikrishna234 <41903289+Ravikrishna234@users.noreply.github.com>
2022-09-14 14:11:14 -0500

    Time: 2597 ms (28.14%), Space: 14.9 MB (76.47%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,5 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        # Method 1    
         my_dict = {}
         for i in range(len(nums)):
             if(target - nums[i] in nums[i+1:]):
@@ -7,4 +9,11 @@ class Solution:
             if(len(my_dict) and target-nums[i] in my_dict.values()):
                 my_dict[i] = nums[i]
             
-        return list(my_dict.keys())
+        # return list(my_dict.keys())
+        
+        # Method 2
+        my_dict = {}
+        for i in range(len(nums)):
+            remaining = target - nums[i]
+            if(remaining in nums[i+1:]):
+                return [i, nums[i+1:].index(remaining) + (i+1)]
